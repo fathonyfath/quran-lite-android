ed52365624d7efd2c327742dcd541385f9f55eb0
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2020-08-05 19:30:45 +0700

    Create preference implementation for saving day night preference

---
+++ app/src/main/java/id/fathonyfath/quran/lite/data/source/preferences/DayNightPreferencesSource.java
@@ -0,0 +1,25 @@
+package id.fathonyfath.quran.lite.data.source.preferences;
+
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+
+public class DayNightPreferencesSource {
+
+    private static final String DAY_NIGHT_PREFERENCE_KEY = "DAY_NIGHT_PREFERENCE_KEY";
+    private final SharedPreferences sharedPreferences;
+
+    DayNightPreferencesSource(SharedPreferences sharedPreferences) {
+        this.sharedPreferences = sharedPreferences;
+    }
+
+    @SuppressLint("ApplySharedPref")
+    public void putValue(String preference) {
+        this.sharedPreferences.edit()
+                .putString(DAY_NIGHT_PREFERENCE_KEY, preference)
+                .commit();
+    }
+
+    public String getValue() {
+        return this.sharedPreferences.getString(DAY_NIGHT_PREFERENCE_KEY, "");
+    }
+}
