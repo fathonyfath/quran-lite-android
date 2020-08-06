c7355dd2b1e6b2385c2b7d75e03774b64a3e45a3
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2020-08-06 18:03:53 +0700

    Update main activity implementation

--- app/src/main/java/id/fathonyfath/quran/lite/MainActivity.java
+++ app/src/main/java/id/fathonyfath/quran/lite/MainActivity.java
@@ -1,31 +1,41 @@
 package id.fathonyfath.quran.lite;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
+import id.fathonyfath.quran.lite.data.ConfigRepository;
 import id.fathonyfath.quran.lite.data.FontProvider;
 import id.fathonyfath.quran.lite.data.QuranRepository;
 import id.fathonyfath.quran.lite.data.font.FontRemoteSource;
 import id.fathonyfath.quran.lite.data.source.disk.QuranDiskSource;
 import id.fathonyfath.quran.lite.data.source.network.QuranNetworkSource;
+import id.fathonyfath.quran.lite.data.source.preferences.DayNightPreferencesSource;
+import id.fathonyfath.quran.lite.models.DayNight;
 import id.fathonyfath.quran.lite.themes.DarkTheme;
 import id.fathonyfath.quran.lite.themes.LightTheme;
 import id.fathonyfath.quran.lite.useCase.FetchAllSurahUseCase;
 import id.fathonyfath.quran.lite.useCase.FetchSurahDetailUseCase;
+import id.fathonyfath.quran.lite.useCase.GetDayNightUseCase;
 import id.fathonyfath.quran.lite.useCase.InstallFontIfNecessaryUseCase;
+import id.fathonyfath.quran.lite.useCase.UseCaseCallback;
 import id.fathonyfath.quran.lite.useCase.UseCaseProvider;
 import id.fathonyfath.quran.lite.utils.ThemeContext;
 import id.fathonyfath.quran.lite.views.MainView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements UseCaseCallback<DayNight> {
 
     public static final String QURAN_REPOSITORY_SERVICE = "MainActivity.QuranRepository";
     public static final String FONT_PROVIDER_SERVICE = "MainActivity.FontProvider";
+    public static final String CONFIG_REPOSITORY_SERVICE = "MainActivity.ConfigRepository";
 
     private QuranRepository quranRepository;
     private FontProvider fontProvider;
+    private ConfigRepository configRepository;
 
     private MainView mainView = null;
+    private long startExecution = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +44,14 @@ public class MainActivity extends Activity {
         initService();
         registerUseCaseFactory();
 
-        this.mainView = new MainView(new ThemeContext(this, new DarkTheme()));
-        setContentView(this.mainView);
+        GetDayNightUseCase useCase = UseCaseProvider.createUseCase(GetDayNightUseCase.class);
+        if (useCase != null) {
+            useCase.setCallback(this);
+            useCase.run();
+        } else {
+            // Fallback to Light Theme
+            showViewWithLightTheme();
+        }
     }
 
     @Override
@@ -60,22 +76,68 @@ public class MainActivity extends Activity {
             return quranRepository;
         } else if (name.equals(FONT_PROVIDER_SERVICE)) {
             return fontProvider;
+        } else if (name.equals(CONFIG_REPOSITORY_SERVICE)) {
+            return configRepository;
         }
         return super.getSystemService(name);
     }
 
+    @Override
+    public void onProgress(float progress) {
+        // Not used
+    }
+
+    @Override
+    public void onResult(DayNight data) {
+        GetDayNightUseCase useCase = UseCaseProvider.getUseCase(GetDayNightUseCase.class);
+        if (useCase != null) {
+            useCase.setCallback(null);
+        }
+
+        UseCaseProvider.clearUseCase(GetDayNightUseCase.class);
+
+        switch (data) {
+            case DAY:
+                showViewWithLightTheme();
+                break;
+            case NIGHT:
+                showViewWithDarkTheme();
+                break;
+        }
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        // Not used
+    }
+
     private void initService() {
+        SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         QuranDiskSource quranDiskSource = new QuranDiskSource(this.getApplicationContext());
         QuranNetworkSource quranNetworkSource = new QuranNetworkSource();
         FontRemoteSource fontRemoteSource = new FontRemoteSource();
+        DayNightPreferencesSource dayNightPreferencesSource = new DayNightPreferencesSource(defaultSharedPreferences);
 
         this.quranRepository = new QuranRepository(quranDiskSource, quranNetworkSource);
         this.fontProvider = new FontProvider(this.getApplicationContext(), fontRemoteSource);
+        this.configRepository = new ConfigRepository(dayNightPreferencesSource);
     }
 
     private void registerUseCaseFactory() {
         UseCaseProvider.registerFactory(InstallFontIfNecessaryUseCase.class, new InstallFontIfNecessaryUseCase.Factory(this.fontProvider));
         UseCaseProvider.registerFactory(FetchAllSurahUseCase.class, new FetchAllSurahUseCase.Factory(this.quranRepository));
         UseCaseProvider.registerFactory(FetchSurahDetailUseCase.class, new FetchSurahDetailUseCase.Factory(this.quranRepository));
+        UseCaseProvider.registerFactory(GetDayNightUseCase.class, new GetDayNightUseCase.Factory(this, this.configRepository));
+    }
+
+    private void showViewWithLightTheme() {
+        this.mainView = new MainView(new ThemeContext(this, new LightTheme()));
+        setContentView(MainActivity.this.mainView);
+    }
+
+    private void showViewWithDarkTheme() {
+        this.mainView = new MainView(new ThemeContext(this, new DarkTheme()));
+        setContentView(MainActivity.this.mainView);
     }
 }
