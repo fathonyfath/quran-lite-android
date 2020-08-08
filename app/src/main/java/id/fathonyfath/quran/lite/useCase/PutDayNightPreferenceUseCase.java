83cf4e3a1eab893a3d87afafafdf19205be5fffa
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2020-08-08 17:01:22 +0700

    Add usecase to update preference

---
+++ app/src/main/java/id/fathonyfath/quran/lite/useCase/PutDayNightPreferenceUseCase.java
@@ -0,0 +1,135 @@
+package id.fathonyfath.quran.lite.useCase;
+
+import android.content.Context;
+import android.content.res.Configuration;
+
+import id.fathonyfath.quran.lite.data.ConfigRepository;
+import id.fathonyfath.quran.lite.models.config.DayNightPreference;
+import id.fathonyfath.quran.lite.utils.scheduler.Schedulers;
+
+public class PutDayNightPreferenceUseCase extends BaseUseCase {
+
+    private final ConfigRepository configRepository;
+    private final Context context;
+
+    private DayNightPreference updateWith;
+    private UseCaseCallback<Boolean> callback;
+
+    public PutDayNightPreferenceUseCase(ConfigRepository configRepository, Context context) {
+        this.configRepository = configRepository;
+        this.context = context;
+    }
+
+    public void setUpdateWith(DayNightPreference updateWith) {
+        this.updateWith = updateWith;
+    }
+
+    public void setCallback(UseCaseCallback<Boolean> callback) {
+        this.callback = callback;
+    }
+
+    @Override
+    protected void task() {
+        if (this.updateWith == null) {
+            postErrorToMainThread(new IllegalStateException("updateWith params should not be null."));
+            return;
+        }
+
+        Schedulers.IO().execute(new Runnable() {
+            @Override
+            public void run() {
+                DayNightPreference current = configRepository.getDayNightPreference();
+                configRepository.putDayNightPreference(updateWith);
+                comparePreviousWithUpdateWith(current, updateWith);
+            }
+        });
+    }
+
+    private void comparePreviousWithUpdateWith(final DayNightPreference previous, final DayNightPreference updateWith) {
+        Schedulers.Computation().execute(new Runnable() {
+            @Override
+            public void run() {
+                // Summary of this branching is that if we receive same preference we will return false
+                // If we receive previous with SYSTEM preference, we will check whether system DayNight
+                // will be the same as updateWith, do the opposite on updateWith with SYSTEM preference
+                if (previous == updateWith) {
+                    postResultToMainThread(false);
+                } else if (previous == DayNightPreference.SYSTEM) {
+                    SystemDayNight currentDayNight = getSystemUiDayNightStatus();
+                    if (updateWith == DayNightPreference.DAY && currentDayNight == SystemDayNight.DAY) {
+                        postResultToMainThread(false);
+                    } else if (updateWith == DayNightPreference.NIGHT && currentDayNight == SystemDayNight.NIGHT) {
+                        postResultToMainThread(false);
+                    } else {
+                        postResultToMainThread(true);
+                    }
+                } else if (updateWith == DayNightPreference.SYSTEM) {
+                    SystemDayNight willResolveInto = getSystemUiDayNightStatus();
+                    if (willResolveInto == SystemDayNight.DAY && previous == DayNightPreference.DAY) {
+                        postResultToMainThread(false);
+                    } else if (willResolveInto == SystemDayNight.NIGHT && previous == DayNightPreference.NIGHT) {
+                        postResultToMainThread(false);
+                    } else {
+                        postResultToMainThread(true);
+                    }
+                }
+            }
+        });
+    }
+
+    private void postResultToMainThread(final boolean isUpdateDifferentFromPrevious) {
+        Schedulers.Main().execute(new Runnable() {
+            @Override
+            public void run() {
+                if (callback != null) {
+                    callback.onResult(isUpdateDifferentFromPrevious);
+                }
+            }
+        });
+    }
+
+    private void postErrorToMainThread(final Exception e) {
+        Schedulers.Main().execute(new Runnable() {
+            @Override
+            public void run() {
+                if (callback != null) {
+                    callback.onError(e);
+                }
+            }
+        });
+    }
+
+    private SystemDayNight getSystemUiDayNightStatus() {
+        int nightModeFlags = this.context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (nightModeFlags) {
+            case Configuration.UI_MODE_NIGHT_NO:
+            case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                // Here we set fallback to Day theme on system
+                return SystemDayNight.DAY;
+            case Configuration.UI_MODE_NIGHT_YES:
+                return SystemDayNight.NIGHT;
+        }
+
+        throw new IllegalStateException("This flow is impossible to get.");
+    }
+
+    private enum SystemDayNight {
+        DAY, NIGHT
+    }
+
+    public static class Factory implements UseCaseFactory<PutDayNightPreferenceUseCase> {
+
+        private final ConfigRepository configRepository;
+        private final Context context;
+
+        public Factory(ConfigRepository configRepository, Context context) {
+            this.configRepository = configRepository;
+            this.context = context;
+        }
+
+        @Override
+        public PutDayNightPreferenceUseCase create() {
+            return new PutDayNightPreferenceUseCase(this.configRepository, this.context);
+        }
+    }
+}
