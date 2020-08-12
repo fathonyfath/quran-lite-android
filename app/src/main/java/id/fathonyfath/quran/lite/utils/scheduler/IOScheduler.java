b4aae11699ed2def307453cd5c00acd061ebc3a1
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2020-08-12 21:45:43 +0700

    Update IO Scheduler to have at least 2 threads running

--- app/src/main/java/id/fathonyfath/quran/lite/utils/scheduler/IOScheduler.java
+++ app/src/main/java/id/fathonyfath/quran/lite/utils/scheduler/IOScheduler.java
@@ -2,10 +2,22 @@ package id.fathonyfath.quran.lite.utils.scheduler;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class IOScheduler implements Scheduler {
 
-    private final Executor executor = Executors.newSingleThreadExecutor();
+    private static final int CORE_POOL_SIZE = 2;
+    private static final int MAXIMUM_POOL_SIZE = 2;
+    private static final int KEEP_ALIVE = 0;
+
+    private final Executor executor = new ThreadPoolExecutor(
+            CORE_POOL_SIZE,
+            MAXIMUM_POOL_SIZE,
+            KEEP_ALIVE,
+            TimeUnit.SECONDS,
+            new LinkedBlockingQueue<Runnable>());
 
     @Override
     public void execute(Runnable runnable) {
