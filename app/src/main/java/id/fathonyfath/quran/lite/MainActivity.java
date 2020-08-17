c1a5cde9a8417418acbb36c9482bcee38623bba4
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2020-08-17 23:42:21 +0700

    Add PutBookmark UseCase

--- app/src/main/java/id/fathonyfath/quran/lite/MainActivity.java
+++ app/src/main/java/id/fathonyfath/quran/lite/MainActivity.java
@@ -5,21 +5,25 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 
+import id.fathonyfath.quran.lite.data.BookmarkRepository;
 import id.fathonyfath.quran.lite.data.ConfigRepository;
 import id.fathonyfath.quran.lite.data.FontProvider;
 import id.fathonyfath.quran.lite.data.QuranRepository;
 import id.fathonyfath.quran.lite.data.font.FontRemoteSource;
 import id.fathonyfath.quran.lite.data.source.disk.QuranDiskSource;
 import id.fathonyfath.quran.lite.data.source.network.QuranNetworkSource;
+import id.fathonyfath.quran.lite.data.source.preferences.BookmarkPreferencesSource;
 import id.fathonyfath.quran.lite.data.source.preferences.DayNightPreferencesSource;
 import id.fathonyfath.quran.lite.models.DayNight;
 import id.fathonyfath.quran.lite.themes.DayTheme;
 import id.fathonyfath.quran.lite.themes.NightTheme;
 import id.fathonyfath.quran.lite.useCase.FetchAllSurahUseCase;
 import id.fathonyfath.quran.lite.useCase.FetchSurahDetailUseCase;
+import id.fathonyfath.quran.lite.useCase.GetBookmarkUseCase;
 import id.fathonyfath.quran.lite.useCase.GetDayNightPreferenceUseCase;
 import id.fathonyfath.quran.lite.useCase.GetDayNightUseCase;
 import id.fathonyfath.quran.lite.useCase.InstallFontIfNecessaryUseCase;
+import id.fathonyfath.quran.lite.useCase.PutBookmarkUseCase;
 import id.fathonyfath.quran.lite.useCase.PutDayNightPreferenceUseCase;
 import id.fathonyfath.quran.lite.useCase.UseCaseCallback;
 import id.fathonyfath.quran.lite.useCase.UseCaseProvider;
@@ -30,10 +34,12 @@ public class MainActivity extends Activity implements UseCaseCallback<DayNight> 
 
     public static final String QURAN_REPOSITORY_SERVICE = "MainActivity.QuranRepository";
     public static final String FONT_PROVIDER_SERVICE = "MainActivity.FontProvider";
+    public static final String BOOKMARK_REPOSITORY_SERVICE = "MainActivity.BookmarkRepository";
     public static final String CONFIG_REPOSITORY_SERVICE = "MainActivity.ConfigRepository";
 
     private QuranRepository quranRepository;
     private FontProvider fontProvider;
+    private BookmarkRepository bookmarkRepository;
     private ConfigRepository configRepository;
 
     private MainView mainView = null;
@@ -82,6 +88,8 @@ public class MainActivity extends Activity implements UseCaseCallback<DayNight> 
             return quranRepository;
         } else if (name.equals(FONT_PROVIDER_SERVICE)) {
             return fontProvider;
+        } else if (name.equals(BOOKMARK_REPOSITORY_SERVICE)) {
+            return bookmarkRepository;
         } else if (name.equals(CONFIG_REPOSITORY_SERVICE)) {
             return configRepository;
         }
@@ -123,10 +131,12 @@ public class MainActivity extends Activity implements UseCaseCallback<DayNight> 
         QuranDiskSource quranDiskSource = new QuranDiskSource(this.getApplicationContext());
         QuranNetworkSource quranNetworkSource = new QuranNetworkSource();
         FontRemoteSource fontRemoteSource = new FontRemoteSource();
+        BookmarkPreferencesSource bookmarkPreferencesSource = new BookmarkPreferencesSource(defaultSharedPreferences);
         DayNightPreferencesSource dayNightPreferencesSource = new DayNightPreferencesSource(defaultSharedPreferences);
 
         this.quranRepository = new QuranRepository(quranDiskSource, quranNetworkSource);
         this.fontProvider = new FontProvider(this.getApplicationContext(), fontRemoteSource);
+        this.bookmarkRepository = new BookmarkRepository(bookmarkPreferencesSource);
         this.configRepository = new ConfigRepository(dayNightPreferencesSource);
     }
 
@@ -134,6 +144,8 @@ public class MainActivity extends Activity implements UseCaseCallback<DayNight> 
         UseCaseProvider.registerFactory(InstallFontIfNecessaryUseCase.class, new InstallFontIfNecessaryUseCase.Factory(this.fontProvider));
         UseCaseProvider.registerFactory(FetchAllSurahUseCase.class, new FetchAllSurahUseCase.Factory(this.quranRepository));
         UseCaseProvider.registerFactory(FetchSurahDetailUseCase.class, new FetchSurahDetailUseCase.Factory(this.quranRepository));
+        UseCaseProvider.registerFactory(GetBookmarkUseCase.class, new GetBookmarkUseCase.Factory(this.bookmarkRepository));
+        UseCaseProvider.registerFactory(PutBookmarkUseCase.class, new PutBookmarkUseCase.Factory(this.bookmarkRepository));
         UseCaseProvider.registerFactory(GetDayNightUseCase.class, new GetDayNightUseCase.Factory(this, this.configRepository));
         UseCaseProvider.registerFactory(GetDayNightPreferenceUseCase.class, new GetDayNightPreferenceUseCase.Factory(this.configRepository));
         UseCaseProvider.registerFactory(PutDayNightPreferenceUseCase.class, new PutDayNightPreferenceUseCase.Factory(this, this.configRepository));
